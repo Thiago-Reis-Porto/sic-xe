120c246dd5d38f2704a208707d09c5657abcd4c1
Dhubleid <42750555+Dhubleid@users.noreply.github.com>
2021-05-07 21:16:08 -0300

    Update Register.java

--- Register.java
+++ Register.java
@@ -2,19 +2,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Register {
-  private Map<String, Integer> register = new HashMap<String, Integer>();
+  private Map<String, Integer> register;
 
   public Register() {
-    register.put("A", 1);
-    register.put("X", 0);
-    register.put("L", 0);
-    register.put("B", 0);
-    register.put("S", 0);
-    register.put("T", 0);
-    register.put("PC", 0);
-    register.put("SW", 0);
+	
+	register = new HashMap<String, Integer>();
+	init();
   };
 
+  private void init() {
+		register.put("A", 1);
+	    register.put("X", 0);
+	    register.put("L", 0);
+	    register.put("B", 0);
+	    register.put("S", 0);
+	    register.put("T", 0);
+	    register.put("PC", 0);
+	    register.put("SW", 0);
+  }
+  
   protected Map<String, Integer> getRegister() {
     return this.register;
   }
